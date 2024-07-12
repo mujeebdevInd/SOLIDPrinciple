962f46af0fdfc3afcefc1b1b5b2162dcaf450eeb
mujeebfive <mujeebfive@gmail.com>
2024-07-12 23:54:38 +0530

    Add project files.

---
+++ SOLIDPrinciple/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+//Before SRP
+//public class Report
+//{
+//    public string GetReportData()
+//    {
+//        // Code to fetch report data
+//        return "Report Data";
+//    }
+
+//    public void PrintReport(string reportData)
+//    {
+//        // Code to print the report
+//        Console.WriteLine("Printing Report: " + reportData);
+//    }
+//}
+
+//After SRP
+public class Report
+{
+    public string GetReportData()
+    {
+        // Code to fetch report data
+        return "Report Data";
+    }
+}
+public class ReportPrinter
+{
+    public void PrintReport(string reportData)
+    {
+        // Code to print the report
+        Console.WriteLine("Printing Report: " + reportData);
+    }
+}
