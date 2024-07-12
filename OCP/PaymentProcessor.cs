0a9278aab665780483ee02e626103e14c9629fe0
mujeebfive <mujeebfive@gmail.com>
2024-07-13 01:52:06 +0530

    OCP

---
+++ OCP/PaymentProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP
+{
+    public class PaymentProcessor
+    {
+        private readonly IPaymentMethod _paymentMethod;
+
+        public PaymentProcessor(IPaymentMethod paymentMethod)
+        {
+            _paymentMethod = paymentMethod;
+        }
+
+        public void ProcessPayment()
+        {
+            _paymentMethod.ProcessPayment();
+        }
+    }
+}
---
+++ OCP/Program.cs
@@ -0,0 +1,65 @@
+﻿
+namespace OCP
+{
+    ////Before OCP
+
+    ////public class PaymentProcessor
+    ////{
+    ////    public void ProcessPayment(string paymentType)
+    ////    {
+    ////        if (paymentType == "CreditCard")
+    ////        {
+    ////            // Process credit card payment
+    ////        }
+    ////        else if (paymentType == "PayPal")
+    ////        {
+    ////            // Process PayPal payment
+    ////        }
+    ////    }
+    ////}
+  
+   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPaymentMethod creditCardPayment = new CreditCardPayment();
+            PaymentProcessor processor1 = new PaymentProcessor(creditCardPayment);
+            processor1.ProcessPayment();
+
+            IPaymentMethod payPalPayment = new PayPalPayment();
+            PaymentProcessor processor2 = new PaymentProcessor(payPalPayment);
+            processor2.ProcessPayment();
+
+            IPaymentMethod bitcoinPayment = new BitcoinPayment();
+            PaymentProcessor processor3 = new PaymentProcessor(bitcoinPayment);
+            processor3.ProcessPayment();
+        }
+    }
+    public class CreditCardPayment : IPaymentMethod
+    {
+        public void ProcessPayment()
+        {
+            // Process credit card payment
+            Console.WriteLine("Processing credit card payment.");
+        }
+    }
+
+    public class PayPalPayment : IPaymentMethod
+    {
+        public void ProcessPayment()
+        {
+            // Process PayPal payment
+            Console.WriteLine("Processing PayPal payment.");
+        }
+    }
+    public class BitcoinPayment : IPaymentMethod
+    {
+        public void ProcessPayment()
+        {
+            // Process Bitcoin payment
+            Console.WriteLine("Processing Bitcoin payment.");
+        }
+    }
+}
