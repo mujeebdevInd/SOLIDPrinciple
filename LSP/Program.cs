f12ab0a52c4063e433a02c768ffa62abf6f8e664
mujeebfive <mujeebfive@gmail.com>
2024-07-13 22:50:07 +0530

    LSP

---
+++ LSP/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IShape rectangle = new Rectangle { Width = 5, Height = 10 };
+            IShape square = new Square { SideLength = 5 };
+
+            Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+            Console.WriteLine($"Square Area: {square.GetArea()}");
+
+            // Both Rectangle and Square can be used interchangeably through IShape
+            PrintArea(rectangle);
+            PrintArea(square);
+        }
+
+        static void PrintArea(IShape shape)
+        {
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
+    }
+
+}
---
+++ LSP/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+    public class Rectangle : IShape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    public class Square : IShape
+    {
+        public int SideLength { get; set; }
+        public int GetArea()
+        {
+            return SideLength * SideLength;
+        }
+    }
+
+}
